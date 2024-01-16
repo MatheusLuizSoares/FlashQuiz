6cb1d16ebbacbd20e8952ebea9e6f126c66be010
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2024-01-16 19:16:36 -0300

    criando as urls

--- FlashQuiz/urls.py
+++ FlashQuiz/urls.py
@@ -1,9 +1,12 @@
-
 from django.contrib import admin
 from django.urls import path
-from django import views
+from cards.views import CardListView, CardCreateView, CardUpdateView, CardDeleteView, CardCompleteView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("home/",)
+    path('admin/', admin.site.urls),
+    path('', CardListView.as_view(), name='card-list'),  # Adicionei um nome à sua view
+    path('card/create/', CardCreateView.as_view(), name='card-create'),
+    path('card/update/<int:pk>/', CardUpdateView.as_view(), name='card-update'),  # Adicionei um parâmetro para o ID do objeto
+    path('card/delete/<int:pk>/', CardDeleteView.as_view(), name='card-delete'),  # Adicionei um parâmetro para o ID do objeto
+    path('card/complete/<int:pk>/', CardCompleteView.as_view(), name='card-complete'),  # Adicionei um parâmetro para o ID do objeto
 ]
--- cards/views.py
+++ cards/views.py
@@ -1,31 +1,31 @@
-from django.shortcuts import render
-from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from .forms import cardsForm
-from .models import cards
-class cardlistView(ListView):
-  model=cards
-  template_name='cards/list.html'
-  context_object_name='cards'
-class cardDetailView(DetailView):
- model=cards
- template_name='cards/detail.html'
- context_object_name='cards'
-class cardCreateView(CreateView):
-   model=cards
-   form_class=cardsForm
-   template_name='cards/create.html'
-   success_url='/cards'
-class cardsUPdateView(UpdateView):
-  model=cards
-  form_class=cardsForm
-  template_name='cards/update.html'
-  success_url='cards'
-class cardsDeleteView(DeleteView):
-  model=cards
-  template_name='cards.html'
-  success_url=reverse_lazy('cardlist')
-  
-  
-  
-   
+from .forms import CardsForm  # Convenção de nome para classes geralmente começa com letra maiúscula
+from .models import Cards
+
+class CardListView(ListView):
+    model = Cards
+    template_name = 'cards/list.html'
+    context_object_name = 'cards'
+
+class CardDetailView(DetailView):
+    model = Cards
+    template_name = 'cards/detail.html'
+    context_object_name = 'card'  # Nome mais apropriado para o objeto individual
+
+class CardCreateView(CreateView):
+    model = Cards
+    form_class = CardsForm
+    template_name = 'cards/create.html'
+    success_url = reverse_lazy('card-list')  # Corrigido o nome da URL reversa
+
+class CardUpdateView(UpdateView):
+    model = Cards
+    form_class = CardsForm
+    template_name = 'cards/update.html'
+    success_url = reverse_lazy('card-list')  # Corrigido o nome da URL reversa
+
+class CardDeleteView(DeleteView):
+    model = Cards
+    template_name = 'cards/delete.html'
+    success_url = reverse_lazy('card-list')  # Corrigido o nome da URL reversa
