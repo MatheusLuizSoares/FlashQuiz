97672991024b8681d38c06a83a98777249d63ca1
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2024-01-17 13:26:41 -0300

    criando as templates

--- FlashQuiz/urls.py
+++ FlashQuiz/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 from cards.views import CardListView, CardCreateView, CardUpdateView, CardDeleteView, CardCompleteView
+from django.urls import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', CardListView.as_view(), name='card-list'),  # Adicionei um nome à sua view
+    path('', CardListView.as_view(), name='card-list'),
     path('card/create/', CardCreateView.as_view(), name='card-create'),
-    path('card/update/<int:pk>/', CardUpdateView.as_view(), name='card-update'),  # Adicionei um parâmetro para o ID do objeto
-    path('card/delete/<int:pk>/', CardDeleteView.as_view(), name='card-delete'),  # Adicionei um parâmetro para o ID do objeto
-    path('card/complete/<int:pk>/', CardCompleteView.as_view(), name='card-complete'),  # Adicionei um parâmetro para o ID do objeto
-]
+    path('card/update/<int:pk>/', CardUpdateView.as_view(), name='card-update'),
+    path('card/delete/<int:pk>/', CardDeleteView.as_view(), name='card-delete'),
+  
+    path('card/complete/<int:pk>/', CardCompleteView.as_view(), name='card-complete'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    ]
--- cards/views.py
+++ cards/views.py
@@ -1,8 +1,10 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .forms import CardsForm  # Convenção de nome para classes geralmente começa com letra maiúscula
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 from .models import Cards
-
 class CardListView(ListView):
     model = Cards
     template_name = 'cards/list.html'
@@ -29,3 +31,26 @@ class CardDeleteView(DeleteView):
     model = Cards
     template_name = 'cards/delete.html'
     success_url = reverse_lazy('card-list')  # Corrigido o nome da URL reversa
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def custom_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
